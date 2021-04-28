8eb9a27d9e96779c85aa677fdb332aad17a875b8
toobythedragontamer <83368314+toobythedragontamer@users.noreply.github.com>
2021-04-28 14:53:01 -0400

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,26 @@
+def menu():
+    print("[1] lb to kg")
+    print("[2] kg to lb")
+    print("[3] exit the program")
+    
+    
+menu()
+option = int(input("enter your option"))
+
+while option != 0:
+    if option  == 1
+    #do option 1 stuff
+    print("you have selected option 1")
+pass
+    elif option  == 2:
+        #do option 2 stuff
+        pass
+    print("you have selected option 2")
+    else 
+    print ("invalid option")
+
+print()    
+menu()
+option = int(input("enter your option"))  
+
+print("thanks for usibg this program")
