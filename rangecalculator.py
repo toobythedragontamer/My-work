d1d013523268cdbdcef6b67bbf493dd7b51a202f
toobythedragontamer <83368314+toobythedragontamer@users.noreply.github.com>
2021-04-29 14:23:16 -0400

    Add files via upload

---
+++ rangecalculator.py
@@ -0,0 +1,11 @@
+def min_max():
+   #manualy imput the range below
+    x_range = (5,6,7,76,26,1,6)
+    print(min(x_range))
+    print(max(x_range))
+    z_range = max(x_range) - min(x_range)
+    print(z_range)
+min_max()
+   
+    
+    
